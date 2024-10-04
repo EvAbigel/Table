66c70dd2819dd21960d75d82d50b4ea56fab9df1
Evbuomwan Abigel Osaretin <evbuomwan.abigel@BOLYAI.LOCAL>
2024-10-04 09:34:09 +0200

    Updated Person

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
 
@@ -26,10 +34,15 @@ const tableheader = document.createElement('thead');
 const tableheaderRow = document.createElement('tr');
 const tableheaderRowLastName = document.createElement('th');
 const tableheaderRowFirstName = document.createElement('th');
+const tableheaderRowMarried = document.createElement('th');
+const tableheaderRowPet = document.createElement('th');
 
 tableheaderRowLastName.innerHTML = "Vezeteknev"
 tableheaderRowFirstName.innerHTML = "Keresztnev"
 tableheaderRowFirstName.colSpan = "2"
+tableheaderRowMarried.innerHTML = "Házas"
+tableheaderRowPet.innerHTML = "Állat"
+
 
 document.body.appendChild(table)
 table.appendChild(tablebody)
@@ -37,6 +50,8 @@ table.appendChild(tableheader)
 tableheader.appendChild(tableheaderRow)
 tableheaderRow.appendChild(tableheaderRowLastName)
 tableheaderRow.appendChild(tableheaderRowFirstName)
+tableheaderRow.appendChild(tableheaderRowMarried)
+tableheaderRow.appendChild(tableheaderRowPet)
 
 for (const person of array){  //person.lastname elérhető
 
@@ -46,7 +61,7 @@ for (const person of array){  //person.lastname elérhető
     {
         const selectedrow = tablebody.querySelector(".selected") //string paraméter, alapvetően tagekkel dolgozik, css-ből veszi át
         e.currentTarget.classList.add('selected')
-        
+
         if (selectedrow != undefined)
             {
                 selectedrow.classList.remove('selected')
@@ -59,24 +74,35 @@ for (const person of array){  //person.lastname elérhető
 
     const td = document.createElement("td")
     const firstname1td = document.createElement("td")
+    const marriedtd = document.createElement("td")
+    const pettd = document.createElement("td")
 
     td.innerHTML = person.lastname
     firstname1td.innerHTML = person.firstname1
+    marriedtd.innerHTML = person.married
+    pettd.innerHTML = person.pet
 
     tablebody.appendChild(tr)
     tr.appendChild(td)
     tr.appendChild(firstname1td)
+    
 
     if (person.firstname2 === undefined){ // 3db =  az típuscheck, 2db logikai vizsgálat
         firstname1td.colSpan = "2"
+        marriedtd.innerHtml = pers.married?"Igen":"Nem"
+
+        tr.appendChild(marriedtd)
+        tr.appendChild(pettd)
     }
     else{
         const firstname2td = document.createElement("td")
         firstname2td.innerHTML = person.firstname2
-        tr.appendChild(firstname2td)
+        marriedtd.innerHtml = pers.married?"Igen":"Nem"
 
+        tr.appendChild(firstname2td)
+        tr.appendChild(marriedtd)
+        tr.appendChild(pettd)
     }
-   
 }
 
 
